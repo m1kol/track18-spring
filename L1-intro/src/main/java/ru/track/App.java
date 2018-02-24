1b7de6d802950a18ce2bd9598306f3617e399fcd
Mikhail <misha.kolesov98@gmail.com>
2018-02-24 12:23:43 +0300

    Done an intro task

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -1,5 +1,10 @@
 package ru.track;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.HttpRequestWithBody;
+
 /**
  * TASK:
  * POST request to  https://guarded-mesa-31536.herokuapp.com/track
@@ -11,21 +16,28 @@ package ru.track;
  */
 public class App {
 
-    public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_GITHUB = "github";
-    public static final String FIELD_EMAIL = "email";
+  public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
+  public static final String FIELD_NAME = "name";
+  public static final String FIELD_GITHUB = "github";
+  public static final String FIELD_EMAIL = "email";
 
-    public static void main(String[] args) throws Exception {
-       // 1) Use Unirest.post()
-        // 2) Get response .asJson()
-        // 3) Get json body and JsonObject
-        // 4) Get field "success" from JsonObject
+  public static void main(String[] args) throws Exception {
+    // 1) Use Unirest.post()
+    // 2) Get response .asJson()
+    // 3) Get json body and JsonObject
+    // 4) Get field "success" from JsonObject
+
+    HttpResponse<JsonNode> postRquest = Unirest.post(URL)
+      .field(FIELD_NAME, "Mikhail")
+      .field(FIELD_EMAIL, "misha.kolesov98@gmail.com")
+      .field(FIELD_GITHUB, "m1kol")
+      .asJson();
+
+    System.out.println (postRquest.getBody().getObject().get("success"));
 
 
+    boolean success = false;
+  }
 
-
-        boolean success = false;
-    }
 
 }
